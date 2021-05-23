05d83797d724c8ac88977422630e638692a1e422
bhatnathan <bhatnathan@gmail.com>
2021-05-23 13:51:21 +0200

    Improving Player Horizontal Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private QuaternionReference worldRotation;
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private QuaternionReference worldRotation; //Reference to the world's rotation
+    [SerializeField] private float horizontalAcceleration; //How fast does the player start moving horizontally
+    [SerializeField] private float horizontalMaxSpeed; //How fast can the player move max in a horizontal direction.
+
+    private Vector3 movement; //The calculated movement we want to apply based on inputs
+
+    //Components
+    private Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //BEGIN TEMP USING THIS FOR DEBUG INPUT
         Vector2 input_direction = Vector2.zero;
+        float input_mag = 1f; //When input is done calculate properly
 
         input_direction += Input.GetKey(KeyCode.UpArrow) ? Vector2.up : Vector2.zero;
         input_direction += Input.GetKey(KeyCode.RightArrow) ? Vector2.right : Vector2.zero;
@@ -24,9 +31,13 @@ public class PlayerMovement : MonoBehaviour
 
         input_direction.Normalize();
 
-        //This is super temp hack because it messes up gravity and all external forces etc and rigidbody manipulation should be in FixedUpdate anyway
-        GetComponent<Rigidbody>().velocity = moveSpeed * InputToWorldDirection(input_direction);
+        movement = input_mag * horizontalMaxSpeed * InputToWorldDirection(input_direction);
         //END TEMP
+    }
+
+    void FixedUpdate()
+    {
+        ApplyHorizontalMovement(movement);
     }
 
     private Vector3 InputToWorldDirection(Vector2 input_direction)
@@ -39,5 +50,18 @@ public class PlayerMovement : MonoBehaviour
         Vector3 flattened_camera_right = camera_right - Vector3.Dot(camera_right, world_up) * world_up;
 
         return Vector3.Normalize(flattened_camera_forward * input_direction.y + flattened_camera_right * input_direction.x);
+    }
+
+    private void ApplyHorizontalMovement(Vector3 movement)
+    {
+        Vector3 world_up = worldRotation.Value * Vector3.up;
+        Vector3 body_horizontal_velocity = body.velocity - Vector3.Dot(body.velocity, world_up) * world_up;
+
+        Vector3 velocity_offset = movement - body_horizontal_velocity;
+
+        float velocity_change_amount = horizontalAcceleration * Time.deltaTime;
+        velocity_change_amount = Mathf.Clamp(velocity_change_amount, 0, velocity_offset.magnitude);
+
+        body.velocity += velocity_offset.normalized * velocity_change_amount;
     }
 }
