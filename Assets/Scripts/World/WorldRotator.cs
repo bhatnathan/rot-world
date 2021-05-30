adfbde89bb268758fb6170fa805be1c9f8cd05b0
bhatnathan <bhatnathan@gmail.com>
2021-05-30 13:27:30 +0200

    World rotates back to last safe position on an essential fall off

--- Assets/Scripts/World/WorldRotator.cs
+++ Assets/Scripts/World/WorldRotator.cs
@@ -1,23 +1,48 @@
 using UnityEngine;
+using System.Linq;
 
 public class WorldRotator : MonoBehaviour
 {
     [Tooltip("Reference to the world's rotation")]
     [SerializeField] private QuaternionVariable worldRotation;
+    [Tooltip("List of all the active dynamic object's datas")]
+    [SerializeField] private DynamicObjectDataList dynamicObjectDatas;
 
     private Quaternion initialRotation;
     private Quaternion latestSafeRotation;
+
+    private bool potentialUnsafeRotation; //we've rotated but we don't know if it's safe yet
 
     private void Awake()
     {
         worldRotation.SetValue(transform.rotation);
         latestSafeRotation = initialRotation = transform.rotation;
+        potentialUnsafeRotation = false;
+    }
+
+    void FixedUpdate()
+    {
+        SetSafePosition();
     }
 
     public void RotateWorld(Axis axis, Rotation direction)
     {        
         Vector3 axis_vector = AxisUtils.AxisToVector(axis);
         worldRotation.SetValue(Quaternion.AngleAxis(direction.Equals(Rotation.Clockwise) ? 90 : -90, worldRotation.Value * axis_vector) * worldRotation.Value);
+        potentialUnsafeRotation = true;
     }
 
+    private void SetSafePosition()
+    {
+        if(potentialUnsafeRotation && dynamicObjectDatas.Values.All(obj => !obj.IsEssential() || obj.IsGrounded()))
+        {
+            latestSafeRotation = worldRotation.Value;
+            potentialUnsafeRotation = false;
+        }
+    }
+
+    public void OnEssentialFallOff()
+    {
+        worldRotation.SetValue(latestSafeRotation);
+    }
 }
