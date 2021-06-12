68ee9700c9d1c30b40e163bbcce4c40045089fc0
Jonas Iacobi <iacobi.jonas@gmail.com>
2021-06-12 10:38:06 +0200

    Add block and goal models and materials

--- Assets/Scripts/World/WorldRotationInputListener.cs
+++ Assets/Scripts/World/WorldRotationInputListener.cs
@@ -55,8 +55,10 @@ public class WorldRotationInputListener : MonoBehaviour
 
     public void OnRotate(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {            
+        Debug.Log("OnRotate");
+        if (context.started)        
+        {
+            Debug.Log("OnRotate Started");
             Rotation rotation = rotationInputMapping.Vector2ToRotation(context.ReadValue<Vector2>());
             InvokeWorldRotation(rotation);
         }
