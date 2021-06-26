0b05518385cb0faddcc20411a5715e5b9500202b
Jonas Iacobi <iacobi.jonas@gmail.com>
2021-06-26 17:23:16 +0200

    Add spikes model and update level editor for decal models

--- Assets/Scripts/Editor/LevelBuilderEditor.cs
+++ Assets/Scripts/Editor/LevelBuilderEditor.cs
@@ -14,9 +14,9 @@ public class LevelBuilderEditor : Editor
         Edit,
         Destroy
     }
-
+    
     int prefabIndex = 0;
-    EditMode editMode = EditMode.None;
+    EditMode editMode = EditMode.None;    
     int levelLayer;
 
     #region Inspector
@@ -31,7 +31,7 @@ public class LevelBuilderEditor : Editor
         }
            
         ShowPrefabDropdown(level_builder);
-        ShowEditMode();
+        ShowEditMode();        
     }        
 
     private void ShowPrefabDropdown(LevelBuilder level_builder)
@@ -50,7 +50,7 @@ public class LevelBuilderEditor : Editor
     private void ShowEditMode()
     {
         GUILayout.Label("Edit Mode");
-        editMode = (EditMode)EditorGUILayout.EnumPopup(editMode, new GUILayoutOption[0]);
+        editMode = (EditMode)EditorGUILayout.EnumPopup(editMode, new GUILayoutOption[0]);                
         GUILayout.Label("Edit Layer");
         levelLayer = EditorGUILayout.LayerField(levelLayer, new GUILayoutOption[0]);        
     }
@@ -72,8 +72,8 @@ public class LevelBuilderEditor : Editor
         RaycastHit hitInfo;
 
         //Return if mouse isn't over an object.                
-        if (!Physics.Raycast(worldRay.origin, worldRay.direction, out hitInfo, Mathf.Infinity, 1 << levelLayer)) { return; }
-        
+        if (!Physics.Raycast(worldRay.origin, worldRay.direction, out hitInfo, Mathf.Infinity, 1 << levelLayer)) { return; }        
+
         DrawPreview(hitInfo);
         
         if (Event.current.type == EventType.MouseUp && Event.current.button == 1)
@@ -81,7 +81,8 @@ public class LevelBuilderEditor : Editor
              switch (editMode)
             {
                 case EditMode.Create:
-                    Create(level_builder, hitInfo);
+                    CreatableObject created = Create(level_builder, hitInfo);
+                    if (created != null && created.Decal) { created.transform.forward = hitInfo.normal; }
                     break;
 
                 case EditMode.Edit:
@@ -117,12 +118,13 @@ public class LevelBuilderEditor : Editor
 
     }
 
-    private void Create(LevelBuilder level_builder, RaycastHit hitInfo)
+    private CreatableObject Create(LevelBuilder level_builder, RaycastHit hitInfo)
     {
         Vector3 spawnPoint = CalculatePositionFromCollider(hitInfo);
-        if(Mathf.Approximately(spawnPoint.sqrMagnitude, 0f)) { return; }
+        if(Mathf.Approximately(spawnPoint.sqrMagnitude, 0f)) { return null; }
         GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(level_builder.GetActivePrefab(), level_builder.transform);
         go.transform.position = spawnPoint;
+        return go.GetComponent<CreatableObject>();
     }
 
     private void Edit(LevelBuilder level_builder, RaycastHit hitInfo)
